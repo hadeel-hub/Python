1016fa2af10c12ac7d4bbb7e53eefb4ee5eb5091
hadeel-hub <hadeela_k2050@yahoo.com>
2022-09-13 16:27:41 +0300

    bank account

--- chaining/code.py
+++ chaining/code.py
@@ -3,21 +3,6 @@ class User:
         self.name= name
         self.account_balance=0
         
-    def make_deposit(self,amount):
-        self.account_balance+= amount
-        return self
-        
-    def make_withdrawl(self,amount):
-        self.account_balance-= amount
-        return self
-    def displayUser_account_balance(self):
-      print(self.account_balance)
-  
-Rabab=User("rabab")
-
-Rabab.make_deposit(3000).make_withdrawl(500).make_deposit(200).make_withdrawl(100)
-Rabab.displayUser_account_balance()
-
-
+    def make_deposit(self,amo
 
     
